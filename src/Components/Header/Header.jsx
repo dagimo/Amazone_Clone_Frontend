c466b4049cc7fffa727d8f90f8af95254eba6450
Tibebe <2dagimo@gmail.com>
2025-05-22 00:46:17 +0300

    Header amendment and lower Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,37 +1,86 @@
 import React from 'react'
-
+import classes from './Header.module.css'
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import LowerHeader from './LowerHeader';
 function Header() {
   return (
     <>
-    <div>
-        {/*logo*/}
-        <a href="">
-            <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
-        </a>
-        {/*delivery*/}
-        <span>
-            {/*icon*/}
-        </span>
-        <div>
-            <p>Dekuvered to</p>
-            <span>Ethiopia</span>
+    <section className=''>
+        
+        <div className={classes.header_container}>
+            <div className={classes.logo_container}>
+            {/*logo section*/}
+            <a href="">
+                <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
+            </a>
+            {/*delivery*/}
+            <div className={classes.delivery}>
+                <span>
+                {/*icon*/}
+                <SlLocationPin />
+                </span>
+                <div>
+                    <p>Delivered to</p>
+                
+
+                    <span>Ethiopia</span>
+                </div>
+            
+
+            </div>
+
+            </div>
+            <div className={classes.search}>
+            {/*search*/}
+            
+            <select name="" id="">
+                <option value="">All</option>
+            </select>
+            <input type="text" name='' id='' placeholder='search product' />
+            {/*icon */}
+            <BsSearch size={25}/>
+        </div>
+
+        
+
+        {/*right side link*/}
+        <div className={classes.order_container}>
+            <a href="" className={classes.language}>
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png" alt="" />
+            <select name='' id=''>
+                <option value="">EN</option>
+            </select>
+
+            </a>
+
+                {/*three components*/}
+            <a href = ''>
+            <p>Sign In</p>
+            <span>Account & Lists</span>
+             </a>
+    {/*orders*/}
+            <a href="">
+            <p>returns</p>
+            <span>& Orders</span>
+            </a>
+    {/*cart*/}
+            <a href='' className={classes.cart}>
+            <BiCart />
+    {/*icon*/}
+            <span>0</span>
+            </a>
+
         </div>
     </div>
-    <div>
-        {/*search*/}
-        <select name="" id="">
-            <option value="">All</option>
-        </select>
-        <input type="text" name='' id='' placeholder='search product' />
-        {/*icon */}
-    </div>
-    {/*right side link*/}
-    <div>
-        <img src="" alt="" />
-        <section>
-            <option value="">EN</option>
-        </section>
-    </div>
+
+        
+        
+    </section>
+    <LowerHeader/>
+
+
     </>
 
     
